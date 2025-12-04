4efff3705b092a5e3e13f3527a2815a4578dc1f3
Benjohn456 <cbjohnston456@gmail.com>
2025-12-04 08:53:24 -0500

    Updated 0.2
    Added hero.js, code.js, and got input box to work.

---
+++ code.js
@@ -0,0 +1,27 @@
+import { Hero } from "./hero";
+
+var input = document.getElementById("input")
+
+toolbox = new Hero();
+
+function warrior (){
+    if (input = warrior)
+        then (strength +5)
+    if (input = Warrior)
+        then (strength +5)
+}
+
+function rogue () {
+    if (input = rogue)
+        then (dexterity +5)
+    if (input = Rogue)
+        then (dexterity +5)
+}
+
+function mage (){
+    if (input = mage)
+        then (intelligence +5)
+    if (input = Mage)
+        then (intelligence +5)
+}
+console.log(Hero)
---
+++ hero.js
@@ -0,0 +1,12 @@
+import { Toolbox } from "./toolbox.js";
+
+export class Hero {
+
+    health = 100
+    strength = 5
+    dexterity = 5
+    intelligence = 5
+
+
+}
+toolbox = new Toolbox();
